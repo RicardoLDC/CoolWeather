c8200b72270007c749dc168210ef67f87e07691a
liu.dachong@ztesoft.com <Ricardoldc@163.com>
2018-09-25 10:35:32 +0800

    Initial commit

---
+++ app/src/main/java/com/coolweather/android/gson/Weather.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Weather {
+    public String status;
+    public Basic basic;
+    public AQI aqi;
+    public Now now;
+    public Suggestion suggestion;
+
+    @SerializedName("daily_forecase")
+    public List<Forecast> forecastList=new ArrayList<>();
+}
